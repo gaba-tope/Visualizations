73bcb0a12ca7b1877e884b37b43866a275a84dbf
gaba-tope <topaize3460@gmail.com>
2024-02-23 23:45:20 +0900

    Interactive map with leaflet package added.

--- Seoul_2023_Dong_income_expenditure/Seoul_Dong_income_exp.R
+++ Seoul_2023_Dong_income_expenditure/Seoul_Dong_income_exp.R
@@ -7,8 +7,6 @@ library(ggrepel)
 library(ggsflabel) # devtools::install_github("yutannihilation/ggsflabel")
 library(sf) # import and use .shp file
 library(shiny)
-library(ggiraph) # Interactive plot
-library(plotly)
 options(scipen = 999) # Disable scientific notation.
 
 ## Fonts---------------------------------------------------
@@ -33,6 +31,22 @@ river_col <- "#56B4E9"
 raw_seoul <- read.csv("./Seoul_income_expendit_dong.csv",fileEncoding = "euc-kr") #https://data.seoul.go.kr/dataList/OA-22168/S/1/datasetView.do
 head(raw_seoul)
 
+# Seoul Dong polygon Data
+map_seoul <- st_read("./Shapes/TL_SCCO_GEMD.shp", options = "ENCODING=euc-kr") # Data last updated in 2022-11
+map_seoul<- sf::st_set_crs(map_seoul, 5179) #5179
+
+EMD_CD_coded <- ddply(map_seoul, .(EMD_CD), function(row){
+  row$dong_code <- (gsub('.{2}$', "", row$EMD_CD))
+})
+
+# Seoul Gu polygon Data
+map_seoul_gu <- st_read("./Shapes/TL_SCCO_SIG.shp", options = "ENCODING=euc-kr")# Map at 2022-11
+map_seoul_gu <- sf::st_set_crs(map_seoul_gu, 5179) #5179
+
+# Seoul River polygon Data
+map_river <- st_read("./Shapes/LSMD_CONT_UJ201_11_202401.shp", options = "ENCODING=euc-kr")
+
+
 
 ## Data Wrangling -----------------------------------------
 # Seoul Income Expenditure per Dong Dataset
@@ -40,8 +54,7 @@ seoul <- raw_seoul
 colnames(seoul) <- c("quarter", "dong_code", "dong_name", "mean_income", "income_interval",
                          "sum_e", "grocery_e", "clothes_e", "life_e", "medical_e",
                          "transport_e", "edu_e", "play_e", "hobby_e", "etc_e", "food_e")
-seoul_20231 <- dplyr::filter(seoul, quarter == 20231)
-seoul_20231 <- as_tibble(seoul_20231)
+seoul_20231 <- dplyr::filter(seoul, quarter == 20231) |> as_tibble()
 seoul_20231$EMD_CD <- as.character(paste0(seoul_20231$dong_code, "00"))
 
 head(seoul_20231)
@@ -57,51 +70,10 @@ seoul_20231_updated[nrow(seoul_20231_updated), 1:3] <- list(20231, 11740526,"상
 seoul_20231_updated[nrow(seoul_20231_updated), 4:16] <- seoul_20231_updated[283, 4:16] # 상일제2동 data is the same as 강일동. 
 seoul_20231_updated[nrow(seoul_20231_updated), 17] <- "1174052600"
 
-
-# Seoul Dong polygon Data
-map_seoul <- st_read("./Shapes/TL_SCCO_GEMD.shp", options = "ENCODING=euc-kr") # Data last updated in 2022-11
-map_seoul<- sf::st_set_crs(map_seoul, 5179) #5179
-
-EMD_CD_coded <- ddply(map_seoul, .(EMD_CD), function(row){
-                          row$dong_code <- (gsub('.{2}$', "", row$EMD_CD))
-                          })
 # Combine sf and data
 combined_data <- left_join(map_seoul, seoul_20231_updated, by = "EMD_CD") # The order of x and y matters.
-combined_data |> filter(is.na(dong_code)) 
+combined_data |> filter(is.na(dong_code)) # To check if any row is missing
 
-# Seoul Gu polygon Data
-map_seoul_gu <- st_read("./Shapes/TL_SCCO_SIG.shp", options = "ENCODING=euc-kr")# Map at 2022-11
-map_seoul_gu <- sf::st_set_crs(map_seoul_gu, 5179) #5179
-
-# Seoul River polygon Data
-map_river <- st_read("./Shapes/LSMD_CONT_UJ201_11_202401.shp", options = "ENCODING=euc-kr")
-
-# Convert sf data to geojson? (Undergoing)---------
-library(geojsonsf)
-geo_dong <- sf_geojson(map_seoul)
-str(geo_dong)
-
-library(geojsonio)
-combined_data_json <- geojson_json(combined_data)
-geojson_write(combined_data_json, file = 
-                "./com.geojson")
-combined_json_simple <- rmapshaper::ms_simplify(combined_data_json)
-
-fig <- plot_ly() |> 
-  add_trace(
-    type = "choropleth",
-    geojson = combined_json_simple,
-    z = mean_income,
-    color_scale = "Viridis",
-  )
-
-fig
-
-class(combined_data)
-plot_ly(combined_data,
-        color = ~mean_income,
-        text = ~paste(EMD_KOR_NM, " : ", mean_income),
-        hoverinfo = "text")
 
 ## Texts --------------------------------------------------
 # Social Info
@@ -113,6 +85,13 @@ cap_map <- glue::glue(
   "<span style='font-family:\"roboto\";'>**Data**</span>: {data_info}<br>
   <span style='font-family:\"roboto\";'>**Graphic**: Tope </span>{social_caption}")
 legend_map <- "평균 소득 (원)"
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>평균 %s원",
+  combined_data$EMD_KOR_NM, format(formattable::comma(combined_data$mean_income, format = 'd'),
+                                   scientific = F)
+) %>% lapply(htmltools::HTML)
+
 
 ## Themes -------------------------------------------------
 # Map Theme--------------------------------------
@@ -164,64 +143,11 @@ map_theme <- theme(
   ), #face = "bold"
   legend.background = element_rect(fill = bg_col)
 )
-# Theme for interactive Map
-map_int_theme <- theme(
-  plot.title.position = "plot", 
-  plot.caption.position = "plot",
-  plot.background = element_rect(fill = bg_col, colour = bg_col),
-  panel.background = element_rect(fill = bg_col, colour = bg_col),
-  panel.grid.major = element_blank(),
-  panel.grid.minor = element_blank(),
-  plot.margin = margin(10, 10, 10, 10),
-  plot.title = element_textbox_simple(
-    colour= text_col,
-    face = "bold",
-    family = main_font,
-    lineheight = 0.5,
-    size = 40,
-    margin = margin(b = 2, t = 2) 
-  ),
-  plot.subtitle = element_textbox_simple(
-    colour = text_col,
-    family = main_font,
-    size = 30,
-    margin = margin(b = 10)
-  ),
-  plot.caption = element_textbox_simple(
-    colour= text_col,
-    lineheight = 0.5,
-    family = main_font,
-    margin = margin(t = 13, b = 5, l = 5),
-    size = 30
-  ),
-  axis.text.x = element_blank(), # No axis text and title.
-  axis.text.y = element_blank(),
-  axis.title.x = element_blank(),
-  axis.title.y = element_blank(),
-  axis.ticks = element_blank(),
-  legend.text = element_text(colour = text_col,
-                             family = num_font,
-                             size = 25,
-                             margin = margin(l = -5)
-  ),#
-  legend.title = element_text(colour = text_col,
-                              family = main_font,
-                              size = 30,
-                              lineheight = 0.5,
-                              face = "bold",
-                              margin = margin(b = -10)
-  ), #face = "bold"
-  legend.background = element_rect(fill = bg_col),
-  legend.key.width = unit(1.5,"cm"),
-  legend.key.height = unit(2,"cm")
-  
-)
-
 
 
 ## Plots --------------------------------------------------
 # Designed Map w/ {ggplot2} ----------------------------------
-seoul_plot <-   ggplot()+
+seoul_plot <-   ggplot() +
   geom_sf(data = combined_data, aes(fill = mean_income), color = major_grid_col, linewidth = 0.1) +
   geom_sf(data = map_seoul_gu, color = text_col, alpha = 0) +
   #geom_sf(data = map_river, fill = river_col, color = river_col, alpha = 0.3) +
@@ -253,81 +179,54 @@ seoul_plot
 ggsave(file="seoul_plot.png", plot = seoul_plot, width = 2000, height= 1400, 
        units = 'px', dpi = 300)
 
-# 3D map?---------
-seoul_plot_3d <-   ggplot()+
-  geom_sf(data = combined_data, aes(fill = mean_income), color = major_grid_col, linewidth = 0.1)+
-  geom_sf(data = map_seoul_gu, color = text_col, alpha = 0 )+
-  scale_fill_distiller(palette = "Purples", direction = 1, labels = scales::label_comma(),
-                       limits = c(2000000, 7500000), breaks = seq(2000000, 7500000, 1000000))+
-  geom_sf_text(data = map_seoul_gu,
-               aes(label = ifelse(!(SIG_KOR_NM %in% c("양천구", "강남구", "동작구", "성북구")), SIG_KOR_NM, "")), 
-               colour = "black")+ # "양천구" text overlaps with the border. "강남구" text is on dark bg.
-  #  "동작구" and "성북구" not centered. 
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "양천구", SIG_KOR_NM, "")), 
-                     colour = "black", nudge_x = -10, nudge_y = -10 # 양천구
-  )+
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "강남구",  SIG_KOR_NM, "")),
-                     colour = "black", nudge_x = -30 , nudge_y = +20
-  )+ # 강남구
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "동작구",  SIG_KOR_NM, "")),
-                     colour = "black", nudge_x = 0 , nudge_y = +30
-  )+ # 동작구
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "성북구",  SIG_KOR_NM, "")),
-                     colour = "black", nudge_x = -10 , nudge_y = -20
-  )
 
- # 성북구
-  labs(title = title_map,
-       caption = cap_map,
-       fill = legend_map)+
-  map_theme
-plot_gg(seoul_plot_3d, width = 10, height = 10,
-        height_aes = "fill", multicore = T, scale = 250, zoom = 0.7, theta = 10,
-        phi = 30, windowsize = c(1200, 1200))
+# Interactive plot using {leaflet} ---------------
+library(leaflet)
+library(htmlwidgets)
+combined_data_l <- st_transform(combined_data, 4326)
+map_seoul_gu_l <- st_transform(map_seoul_gu, 4326)
+
+pal <- colorNumeric(palette = "Purples", domain = combined_data_l$mean_income)
+cent_seoul_gu<- st_centroid(map_seoul_gu_l) # Cenroid of the polygon, where Gu-Name will be labeled
 
 
-# Interactive Map with {ggiraph}------------------------------
-seoul_plot_int <-   ggplot()+
-  geom_sf_interactive(data = combined_data, aes(fill = mean_income),
-                      tooltip =paste(combined_data$EMD_KOR_NM, ":", comma(combined_data$mean_income),"원"),
-                      color = lighter_text_col, linewidth = 0.5)+ 
-  geom_sf(data = map_seoul_gu, color = text_col, alpha = 0, linewidth = 1 )+
-  scale_fill_distiller(palette = "Purples", direction = 1, labels = scales::label_comma(),
-                       limits = c(2000000, 7500000), breaks = seq(2000000, 7500000, 1000000))+
-  geom_sf_text(data = map_seoul_gu,
-               aes(label = ifelse(!(SIG_KOR_NM %in% c("양천구", "강남구", "동작구", "성북구")), SIG_KOR_NM, "")), 
-               colour = "black", size = 6)+ # "양천구" text overlaps with the border. "강남구" text is on dark bg.
-  #  "동작구" and "성북구" not centered. 
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "양천구", SIG_KOR_NM, "")), 
-                     colour = "black", nudge_x = -10, nudge_y = -10, size = 6 # 양천구
-  )+
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "강남구",  SIG_KOR_NM, "")),
-                     colour = "black", nudge_x = -40 , nudge_y = +30, size = 6
-  )+ # 강남구
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "동작구",  SIG_KOR_NM, "")),
-                     colour = "black", nudge_x = 0 , nudge_y = +30, size = 6
-  )+ # 동작구
-  geom_sf_text_repel(data = map_seoul_gu, aes(label = ifelse(SIG_KOR_NM == "성북구",  SIG_KOR_NM, "")),
-                     colour = "black", nudge_x = -10 , nudge_y = -20, size = 6
-  )+ # 성북구
-  labs(title = title_map,
-       caption = cap_map,
-       fill = legend_map)+
-  map_int_theme
+l <- leaflet() |> addTiles() |> 
+  addPolygons(data = map_seoul_gu_l,
+              color = "black", weight = 5,
+              fillColor = "transparent",
+              fillOpacity = 1, group = "gu") |> 
+  addLabelOnlyMarkers(data = cent_seoul_gu, label =  ~SIG_KOR_NM, 
+                      labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T,
+                                                  style = list(
+                                                    "font-family" = "noto-sans",
+                                                    "font-style" = "bold",
+                                                    "font-size" = "15px"
+                                                  ))
+  ) |> 
+  addPolygons(data = combined_data_l,
+              color = "#1a1a1a", weight = 1, dashArray = "3",
+              fillColor = ~ pal(mean_income), fillOpacity = 0.8,
+              highlightOptions = highlightOptions(
+                weight = 3,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.8,
+                bringToFront = T
+              ),
+              label = labels,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px", direction = "auto"
+              )
+  ) |> 
+  addLegend(data = combined_data_l,
+            pal = pal, values = ~mean_income, opacity = 0.8,
+            title = "평균 소득 (원)")
 
-tooltip_css <- "background-color:#d8118c;color:white;padding:5px;border-radius:3px;"
-
-girafe_map_int <- girafe(ggobj = seoul_plot_int,
-       width_svg = 35, height_svg = 17) |> girafe_options(opts_hover(css = "fill:yellow;stroke:black;stroke-width:3px;"),
-                                                 opts_tooltip(css = tooltip_css, opacity = 1),
-                                                 opts_zoom(min = .7, max = 2)) 
-htmlwidgets::saveWidget(girafe_map_int, file="girafe_map_int.html")
+l
+saveWidget(l, file = "map_leaflet.html") 
 
 
-# Interactive plot using {plotly} ---------------
-ggplotly(seoul_plot)
-
-# Google map ----------------
 
 ## Shiny (For Practice) -------------------------------------------
 plotdf <- data.frame()
